86544bf97195a1b2170b33a5704eecd43475abcd
Leonardo Zani de Souza <leonardozanidev@gmail.com>
2026-06-07 18:10:04 -0300

    feat: how to section in home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -28,6 +28,44 @@ const Home = () => {
         <LinkButton text="Comece grátis" to="/register" />
       </div>
       <ExampleCV />
+      <div className="flex flex-col text-center my-10">
+        <h3 className="font-bold mb-5">COMO FUNCIONA?</h3>
+        <div className="flex">
+          <div className="flex flex-col items-center justify-center gap-2 max-w-50">
+            <div className="flex items-center justify-center h-10 w-10 border text-center bg-[#7f77dd]/30 text-[#7f77dd] font-bold p-3 rounded-full">
+              1
+            </div>
+            <h4 className="text-sm font-bold">Preencha</h4>
+            <p className="text-sm">
+              Informe seus dados ou importe um currículo existente
+            </p>
+          </div>
+          <div className="flex items-center p-3">
+            <span>→</span>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-2 max-w-50">
+            <div className="flex items-center justify-center h-10 w-10 border text-center bg-[#7f77dd]/30 text-[#7f77dd] font-bold p-3 rounded-full">
+              2
+            </div>
+            <h4 className="text-sm font-bold">IA Analisa</h4>
+            <p className="text-sm">
+              Receba sugestões de melhorias personalizadas
+            </p>
+          </div>
+          <div className="flex items-center p-3">
+            <span>→</span>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-2 max-w-50">
+            <div className="flex items-center justify-center h-10 w-10 border text-center bg-[#7f77dd]/30 text-[#7f77dd] font-bold p-3 rounded-full">
+              3
+            </div>
+            <h4 className="text-sm font-bold">Baixe o PDF</h4>
+            <p className="text-sm">
+              Exporte um currículo profissional pronto pra enviar
+            </p>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
